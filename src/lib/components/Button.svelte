57c21bf50c18555462e167dd44f6bd55eb181e81
pnm122 <pnmartin02@gmail.com>
2024-09-11 01:44:27 -0400

    Create hero section, button element, update theming

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+	import createClasses from "$utils/createClasses";
+	import gsap from "gsap";
+	import { Observer } from "gsap/dist/Observer";
+	import type { Snippet } from "svelte";
+
+  interface Props {
+    size?: 'small' | 'medium' | 'large'
+    style?: 'primary'
+    type?: 'link' | 'button'
+    href?: string
+    onClick?: (e: MouseEvent) => void
+    class?: string
+    children: Snippet
+  }
+
+  const {
+    size = 'medium',
+    style = 'primary',
+    type = 'button',
+    href = '#',
+    onClick,
+    class: className,
+    children
+  }: Props = $props()
+
+  const classes = createClasses({
+    'button': true,
+    [`button--${style}`]: true,
+    'button--small': size === 'small',
+    'button--medium': size === 'medium',
+    'button--large': size === 'large',
+    ...(className ? { [className]: true } : {})
+  })
+
+  let button = $state<HTMLElement>()
+  let buttonBackground = $state<HTMLElement>()
+  // Needed to guarantee that the onHoverEnd animation happens after the last onMove animation
+  let isHovering = $state(false)
+
+  function scaleOffset(offset: number) {
+    return offset >= 0 ? (
+      offset / (1 + (offset / 10))
+    ) : (
+      -1 * ((-1 * offset) / (1 + ((-1 * offset) / 10)))
+    )
+  }
+
+  $effect(() => {
+    const ctx = gsap.context(() => {
+      Observer.create({
+        target: button,
+        type: 'pointer',
+        onMove: ({ x, y }) => {
+          if(!isHovering) return
+
+          const { x: bgX, y: bgY, width, height } = buttonBackground?.getBoundingClientRect()!
+          const xOffset = x! - bgX - (width / 2)
+          const yOffset = y! - bgY - (height / 2)
+          gsap.to(buttonBackground!, {
+            x: scaleOffset(xOffset),
+            y: scaleOffset(yOffset),
+            ease: 'elastic.out',
+            duration: 0.5
+          })
+        },
+        onHover: () => {
+          isHovering = true
+        },
+        onHoverEnd: () => {
+          isHovering = false
+          gsap.to(buttonBackground!, {
+            x: 0,
+            y: 0,
+            ease: 'elastic.out',
+            duration: 0.5
+          })
+        }
+      })
+    })
+
+    return () => ctx.revert()
+  })
+</script>
+
+{#if type === 'button'}
+  <button
+    bind:this={button}
+    class={classes}
+    onclick={e => onClick && onClick(e)}>
+    <div bind:this={buttonBackground} class='button__background'></div>
+    <span class='button__inner'>{@render children()}</span>
+  </button>
+{:else}
+  <a
+    bind:this={button}
+    href={href}
+    class={classes}
+    onclick={e => onClick && onClick(e)}>
+    <div bind:this={buttonBackground} class='button__background'></div>
+    <span class='button__inner'>{@render children()}</span>
+  </a>
+{/if}
+
+<style lang="scss">
+  @import '$scss/variables';
+  @import '$scss/mixins';
+
+  .button {
+    color: var(--button-inner-color);
+    border-radius: 999px;
+    position: relative;
+    font-weight: 500;
+    width: fit-content;
+    text-transform: lowercase;
+    outline: none;
+    @include centered;
+
+    &:focus-visible &__background {
+      @include focus;
+    }
+
+    &--primary {
+      --button-color: #{$primary};
+      --button-inner-color: #{$primary-dark};
+    }
+
+    &--small {
+      height: 32px;
+      padding-inline: 12px;
+    }
+
+    &--medium {
+      height: 40px;
+      padding-inline: 18px;
+    }
+
+    &--large {
+      height: 48px;
+      padding-inline: 24px;
+      font-size: $font-size-10;
+    }
+
+    &__background {
+      position: absolute;
+      inset: 0;
+      z-index: 0;
+      border-radius: 999px;
+      background-color: var(--button-color);
+      pointer-events: none;
+    }
+
+    &__inner {
+      position: relative;
+      z-index: 1;
+      color: inherit;
+    }
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import '$scss/index.scss';
+	import gsap from 'gsap';
+	import { Observer } from 'gsap/dist/Observer';
+
+  if(browser) {
+    gsap.registerPlugin(Observer)
+  }
 </script>
 
 <slot></slot>
