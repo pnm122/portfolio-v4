b8b6ce0fda4fe64113ab65e3cb006a6872fac08a
pnm122 <pnmartin02@gmail.com>
2024-09-11 13:12:28 -0400

    Navigation appear/disappear animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import Navigation from '$components/Navigation.svelte';
 	import '$scss/index.scss';
 	import gsap from 'gsap';
 	import { Observer } from 'gsap/dist/Observer';
@@ -9,4 +10,5 @@
   }
 </script>
 
+<Navigation />
 <slot></slot>
