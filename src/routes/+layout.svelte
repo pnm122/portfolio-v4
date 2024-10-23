be4178a2f7c6bec15f7382760cce256dd8e1cc07
pnm122 <pnmartin02@gmail.com>
2024-10-23 16:19:05 -0400

    Improve view transitions depending on which pages the user is going to/coming from

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,37 @@
 		gsap.registerPlugin(Observer, ScrollTrigger, ScrollToPlugin)
 	}
 
+  function isCaseStudyPage(path: string | undefined) {
+    return path && path.startsWith('/case-study')
+  }
+
+  function isHomePage(path: string | undefined) {
+    return path === '/'
+  }
+
+  // An ID maintained by svelte that lets me determine if the user is going forward or backward in navigation
+  let currentPageHistoryId = $state<number>(browser ? history.state['sveltekit:history'] : -1)
+
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return
+
+    const nextPageHistoryId = history.state['sveltekit:history']
+    const fromPath = navigation.from?.url.pathname
+    const toPath = navigation.to?.url.pathname
+
+    if(!isHomePage(fromPath) && isHomePage(toPath)) {
+      document.querySelector('html')!.id = 'to-home'
+    } else if(!isCaseStudyPage(fromPath) && isCaseStudyPage(toPath)) {
+      document.querySelector('html')!.id = 'to-case-study'
+    } else if(isCaseStudyPage(fromPath) && isCaseStudyPage(toPath) && nextPageHistoryId >= currentPageHistoryId) {
+      document.querySelector('html')!.id = 'between-case-studies-forward'
+    } else if(isCaseStudyPage(fromPath) && isCaseStudyPage(toPath) && nextPageHistoryId < currentPageHistoryId) {
+      document.querySelector('html')!.id = 'between-case-studies-backward'
+    } else {
+      document.querySelector('html')!.id = ''
+    }
+
+    currentPageHistoryId = nextPageHistoryId
 
 		return new Promise((res) => {
 			document.startViewTransition(async () => {
